271f3f7bb0e12986256988159b3f77d075964968
Víctor Díaz <diaz.victorm@pucp.edu.pe>
2022-04-19 20:13:23 -0500

    Creación de vistas (sin datos) y funciones del controlador Servicio

--- src/main/java/com/example/lab3grupo1/controller/ServicioController.java
+++ src/main/java/com/example/lab3grupo1/controller/ServicioController.java
@@ -1,4 +1,60 @@
 package com.example.lab3grupo1.controller;
 
+import com.example.lab3grupo1.entity.Servicio;
+import com.example.lab3grupo1.repository.ResponsableRepository;
+import com.example.lab3grupo1.repository.ServicioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.*;
+
+@Controller
+@RequestMapping("/servicio")
 public class ServicioController {
+
+    @Autowired
+    ServicioRepository servicioRepository;
+
+    @Autowired
+    ResponsableRepository responsableRepository;
+
+    @GetMapping(value = {"/lista", ""})
+    public String listarServicios(Model model) {
+        List<Servicio> lista = servicioRepository.findAll();
+        model.addAttribute("servicioLista", lista);
+        return "servicio/lista";
+    }
+
+    @GetMapping("/nuevo")
+    public String nuevoServicio(Model model) {
+        model.addAttribute("listaResponsable", responsableRepository.findAll());
+        return "servicio/nuevo";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarServicio(Servicio servicio, RedirectAttributes attr) {
+        if (servicio.getId() == 0) {
+            attr.addFlashAttribute("msg", "Servicio creado exitosamente");
+        } else {
+            attr.addFlashAttribute("msg", "Servicio actualizado exitosamente");
+        }
+        servicioRepository.save(servicio);
+        return "redirect:/servicio/lista";
+    }
+
+    @GetMapping("/editar")
+    public String editarServicio(Model model, @RequestParam("id") int id) {
+        Optional<Servicio> optionalServicio = servicioRepository.findById(id);
+        if (optionalServicio.isPresent()) {
+            Servicio servicio = optionalServicio.get();
+            model.addAttribute("servicio", servicio);
+            return "servicio/editar";
+        } else {
+            return "redirect:/servicio/lista";
+        }
+    }
+
 }
