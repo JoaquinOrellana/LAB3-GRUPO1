1f057440f362c42f3d87a2a7136bf382226ad8a4
Joaquin Orellana <joaquin.orellana@pucp.edu.pe>
2022-04-19 19:23:40 -0500

    Initial commit

---
+++ src/main/java/com/example/lab3grupo1/Lab3Grupo1Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3grupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Grupo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3Grupo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3grupo1/Lab3Grupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3grupo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3Grupo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
